d9edf91fd7f70a03e740aa4fce15b46342241860
daniel.aguil <daguil@untdf.edu.ar>
2023-05-09 22:57:15 -0300

    se avanza la estructura

---
+++ src/modulo2/ejercicios/ej_01/Ejercicio.java
@@ -0,0 +1,14 @@
+package modulo2.ejercicios.tp1;
+
+/**
+ * Escribir un programa que muestre tu nombre por pantalla.
+ */
+
+public class Ejercicio {
+
+    public static void main(String[] args) {
+
+        //
+        System.out.println("Daniel E, AGUIL MALLEA");
+    }
+}
---
+++ src/modulo2/ejercicios/ej_02/Ejercicio.java
@@ -0,0 +1,8 @@
+package modulo2.ejercicios.tp2;
+
+/**
+ * Modificar el programa anterior para que además muestre tu dirección y tu número de teléfono.
+ * Asegurarse de que los datos se muestran en líneas separadas.
+ */
+public class Ejercicio {
+}
