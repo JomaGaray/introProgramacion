7b583e831db330ffe4a263a8f60cfcc7416f7189
Jose Garay <josemgaray14@gmail.com>
2023-06-06 13:38:14 -0300

    Update and rename ejercicio to ejercicio.java

---
+++ src/modulo2/ejercicios/ej_06/ejercicio.java
@@ -0,0 +1,28 @@
+public class Ejercicio{
+
+  public static void main(String[] args) {
+  
+        System.out.println("      x \n");
+        System.out.println("     xxx\n");
+        System.out.println("    xxxxx\n");
+        System.out.println("   xxxxxxx\n");
+        System.out.println("  xxxxxxxxx\n");
+
+
+        System.out.println("      x\n" +
+                           "     xxx\n" +
+                           "    xxxxx\n" +
+                           "   xxxxxxx\n" +
+                           "  xxxxxxxxx");
+        
+      System.out.println("""
+                        x
+                       xxx
+                      xxxxx
+                     xxxxxxx
+                    xxxxxxxxx\
+                  """);
+
+  }
+
+}
