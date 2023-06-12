e629534b1924559c34a087a2f2c67374506e965d
Jose Garay <josemgaray14@gmail.com>
2023-06-12 18:10:56 -0300

    Create ejercicio5.java

---
+++ src/modulo3/ejercicio5.java
@@ -0,0 +1,16 @@
+public class EjerciciosTp3 {
+    public static void main(String[] args) {
+        /* ejercicio 5
+        final  int valorDolar = 460;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese la cantidad de dolares: ");
+        String dolares = sc.nextLine();
+
+        int nroDolares = Integer.valueOf(dolares);
+
+        System.out.println("la conversion es : " + nroDolares*valorDolar );
+
+        */
+        }
+}
