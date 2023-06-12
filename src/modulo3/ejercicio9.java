40a282d1c7fe5592db68b05cd8e9eea5298e969b
Jose Garay <josemgaray14@gmail.com>
2023-06-12 18:15:11 -0300

    Create ejercicio9.java

---
+++ src/modulo3/ejercicio9.java
@@ -0,0 +1,18 @@
+public class EjerciciosTp3 {
+    public static void main(String[] args) {
+        /* ejercicio 9
+        final double gb =  0.001;
+        final int kb = 1000;
+        final int bt = 1000000; //bytes
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("INgrese un valor en mb: ");
+
+        int valorEnMb = Integer.parseInt(sc.nextLine());
+
+        System.out.println("valor en Mb -->"+ valorEnMb+"; valor en Gb -->" + (valorEnMb * gb) );
+        System.out.println("valor en Mb -->"+ valorEnMb+"; valor en Kb -->" + (valorEnMb * kb) );
+        System.out.println("valor en Mb -->"+ valorEnMb+"; valor en Bytes -->" + (valorEnMb * bt) );
+        */
+        }
+ }
