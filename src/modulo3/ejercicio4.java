12a77c8fc34e22fa8aba2e7917e873d1e9323bb6
Jose Garay <josemgaray14@gmail.com>
2023-06-12 18:09:48 -0300

    Create ejercicio4.java

---
+++ src/modulo3/ejercicio4.java
@@ -0,0 +1,18 @@
+public class EjerciciosTp3 {
+    public static void main(String[] args) {  
+      
+      /* ejercicio 4
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero de 4 digitos: ");
+
+        String nro = sc.nextLine();
+
+        System.out.println("el nro es: ");
+        System.out.println(nro.charAt(0));
+        System.out.println(nro.charAt(1));
+        System.out.println(nro.charAt(2));
+        System.out.println(nro.charAt(3));
+      */
+    }
+}
